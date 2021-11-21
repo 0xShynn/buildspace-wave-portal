a137e1f047f7d1c6cc4e9619b6277e50b13fa732
Antonin Nhek <a.nhek@icloud.com>
2021-11-21 20:31:58 +0100

    section 2: setup a basic react app, setup Metamask

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,4 +1,4 @@
-import { Box, Flex, Heading, Link, SimpleGrid, Text } from '@chakra-ui/layout'
+import { Box, Flex, Heading } from '@chakra-ui/layout'
 import { NextSeo } from 'next-seo'
 import Image from 'next/image'
 
@@ -17,49 +17,7 @@ export default function Home() {
         py="12"
         px="6"
       >
-        <Heading as="h1">Next Chakra UI Starter</Heading>
-
-        <Text mb="12">
-          Get started by editing <code>pages/index.js</code>
-        </Text>
-
-        <SimpleGrid columns={[1, 2]} spacing={12} maxW="600px">
-          <Link href="https://nextjs.org/docs">
-            <Heading as="h3" fontSize="lg">
-              Documentation &rarr;
-            </Heading>
-            <Text>
-              Find in-depth information about Next.js features and API.
-            </Text>
-          </Link>
-
-          <Link href="https://nextjs.org/learn">
-            <Heading as="h3" fontSize="lg">
-              Learn &rarr;
-            </Heading>
-            <Text>
-              Learn about Next.js in an interactive course with quizzes!
-            </Text>
-          </Link>
-
-          <Link href="https://github.com/vercel/next.js/tree/master/examples">
-            <Heading as="h3" fontSize="lg">
-              Examples &rarr;
-            </Heading>
-            <Text>
-              Discover and deploy boilerplate example Next.js projects.
-            </Text>
-          </Link>
-
-          <Link href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app">
-            <Heading as="h3" fontSize="lg">
-              Deploy &rarr;
-            </Heading>
-            <Text>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </Text>
-          </Link>
-        </SimpleGrid>
+        <Heading as="h1">Wave Portal 👋</Heading>
       </Flex>
 
       <Box role="contentinfo">
