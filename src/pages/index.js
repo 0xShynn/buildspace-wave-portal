e68e1fe2948fa254d28ddc05767f055f7e9f3048
Antonin Nhek <a.nhek@icloud.com>
2021-11-22 02:06:30 +0100

    section 2.3: connect our wallet to our web app

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,8 +1,65 @@
-import { Box, Flex, Heading } from '@chakra-ui/layout'
+import { useEffect, useState } from 'react'
+
+import { Box, Button, Flex, Heading, VStack } from '@chakra-ui/react'
 import { NextSeo } from 'next-seo'
 import Image from 'next/image'
 
 export default function Home() {
+  const [currentAccount, setCurrentAccount] = useState('')
+  const [isLoading, setIsLoading] = useState(false)
+
+  const checkIfWalletIsConnected = async () => {
+    try {
+      const { ethereum } = window
+
+      if (!ethereum) {
+        console.log('Make sure you have MetaMask!')
+        return
+      } else {
+        console.log('We have ethereum object', ethereum)
+      }
+
+      const accounts = await ethereum.request({ method: 'eth_accounts' })
+
+      if (accounts.length !== 0) {
+        const account = accounts[0]
+        console.log('Found an authorized account: ', account)
+        setCurrentAccount(account)
+      } else {
+        console.log('No authorized account found.')
+      }
+    } catch (error) {
+      console.log(error)
+    }
+  }
+
+  const connectWallet = async () => {
+    try {
+      const { ethereum } = window
+
+      if (!ethereum) {
+        alert('Get MetaMask!')
+        return
+      }
+
+      const accounts = await ethereum.request({ method: 'eth_requestAccounts' })
+
+      console.log('Connected', accounts[0])
+      setCurrentAccount(accounts[0])
+    } catch (error) {
+      console.log(error)
+    }
+  }
+
+  useEffect(() => {
+    const onLoad = async () => {
+      setIsLoading(true)
+      await checkIfWalletIsConnected()
+      setIsLoading(false)
+    }
+    onLoad()
+  }, [])
+
   return (
     <Box>
       {/* Edit the Head info */}
@@ -18,6 +75,15 @@ export default function Home() {
         px="6"
       >
         <Heading as="h1">Wave Portal 👋</Heading>
+        <VStack>
+          <Button colorScheme="blue" size="lg" mt="8">
+            Wave at me
+          </Button>
+
+          {!currentAccount && !isLoading && (
+            <Button onClick={connectWallet}>Connect Wallet</Button>
+          )}
+        </VStack>
       </Flex>
 
       <Box role="contentinfo">
